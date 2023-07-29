c309cf0a01db966ce58797d67593e4ce31a65ac9
leducmanhimk <lemanh8456@gmail.com>
2023-07-29 22:32:53 +0700

    cap nhat model cho chuc nang xoa cai thien code thua

--- src/main/java/com/example/wedmenusuggestions_amilymeals/VNUA/controllers/RecipeController.java
+++ src/main/java/com/example/wedmenusuggestions_amilymeals/VNUA/controllers/RecipeController.java
@@ -5,19 +5,14 @@ import com.example.wedmenusuggestions_amilymeals.VNUA.models.FileUploadUtil;
 import com.example.wedmenusuggestions_amilymeals.VNUA.models.Recipe;
 import com.example.wedmenusuggestions_amilymeals.VNUA.services.RecipeService;
 import jakarta.validation.Valid;
-import org.apache.tomcat.util.http.fileupload.FileUpload;
-import org.codehaus.groovy.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 public class RecipeController {
@@ -26,13 +21,13 @@ public class RecipeController {
     private RecipeService recipeService;
 
 
-    @GetMapping("/FoodBrower")
+    @GetMapping("recipe/foodbrower")
     public String viewHomepage(Model model){
         model.addAttribute("listrecipes",recipeService.getRecipe());
-        return "FoodBrowser";
+        return "foodBrowser";
     }
 
-    @GetMapping("/saveRecipe")
+    @GetMapping("recipe/saveRecipe")
     public String showNewRecipeForm(Model model){
         // create model attribute to bind form data
         Recipe recipe = new Recipe();
@@ -40,7 +35,7 @@ public class RecipeController {
         return "form_save_recipe";
     }
 
-    @PostMapping("/saveRecipe")
+    @PostMapping("recipe/saveRecipe")
     public String saveRecipe(@RequestParam("imagefile")MultipartFile multipartFile,@Valid @ModelAttribute("recipe")
       Recipe recipe, BindingResult result) throws IOException {
         if (result.hasErrors()) {
@@ -53,10 +48,10 @@ public class RecipeController {
         FileUploadUtil.saveFile(uploadDir, filename, multipartFile);
         // save recipe to database
         recipeService.saveRecipe(recipe);
-        return "redirect:/FoodBrower";
+        return "redirect:/recipe/foodbrower";
     }
 
-    @GetMapping("/showFormForUpdate/{id}")
+    @GetMapping("recipe/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
 
         // get recipe from the service
@@ -67,7 +62,7 @@ public class RecipeController {
         return "update_recipe";
     }
 
-    @GetMapping("/recipe/{id}")
+    @GetMapping("/recipe/detail/{id}")
     public String showDetailRecipe(@PathVariable(value = "id")long id,Model model){
         Recipe recipe = recipeService.getRecipeById(id);
 
@@ -79,6 +74,6 @@ public class RecipeController {
     public String deleteRecipe(@PathVariable(value = "id") long id) {
         // call delete employee method
         this.recipeService.deleteRecipeById(id);
-        return "redirect:/FoodBrower";
+        return "redirect:/recipe/foodbrower";
     }
 }
